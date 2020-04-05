e52eeac8cfde63689e3cc3caedeb5ceada0c3e52
Claudio Resende <claudiolr@gmail.com>
2020-04-05 17:58:15 -0300

    atualizações no código

--- dtiTeste.R
+++ dtiTeste.R
@@ -17,7 +17,7 @@ legFrequencia <- c("Nunca","Quase nunca","Às vezes","Quase sempre","Sempre")
 
 
 # Tratamento da base ------------------------------------------------------
-setwd("C:/Users/Claudio/OneDrive/CienciaDados")
+setwd("~/GitHub/geral")
 base <- read.csv("wiki4HE.csv", sep=";")
 
 
@@ -40,34 +40,8 @@ base$ENJ1Recod <- as.factor(ifelse(is.na(base$ENJ1), NA,
 base$ENJ2Recod <- as.factor(ifelse(is.na(base$ENJ2), NA,
                                    ifelse(base$ENJ2 %in% (1:3), 0, 1)))
 
-
-
-
-
 # Análises ----------------------------------------------------------------
 
-### Gráficos não utilizados
-plot(table(base$USERWIKI, base$GENDER), col = brewer.pal(8, "Blues")[5:8])
-barplot(table(base$PU1), space = 0.02, border = NA,
-        col = brewer.pal(8, "Blues")[4:8],
-        args.legend = list(x = "topleft"),
-        names.arg = legConcordancia,
-        main = paste("O uso da Wikipedia facilita para os estudantes", "\n","o desenvolvimento de novas habilidades"), font = 1,
-        las = 1)
-
-barplot(table(base$PU2), space = 0.02, border = NA,
-        col = brewer.pal(8, "Greens")[4:8],
-        args.legend = list(x = "topleft"),
-        names.arg = legConcordancia,
-        main = paste("O uso da Wikipedia aprimora", "\n","o aprendizado dos estudantes"), font = 1,
-        las = 1)
-
-barplot(table(base$PU3), space = 0.02, border = NA,
-        col = brewer.pal(8, "Reds")[4:8],
-        args.legend = list(x = "topleft"),
-        names.arg = legConcordancia,
-        main = paste("A Wikipedia é útil para o ensino"), font = 1,
-        las = 1)
 
 
 ### Descrição
@@ -84,15 +58,27 @@ ggplot(base, aes(x = as.factor(GENDER), fill=as.factor(GENDER))) +
 
 
 # Perfil de uso
-ggplot(base, aes(x = as.factor(USERWIKI), fill = as.factor(USERWIKI))) + 
-     geom_bar(na.rm = TRUE) + 
-     labs(title = "Perfil de uso da Wikipedia",
-          x = "", y = NULL) + 
-     scale_fill_manual(values = c("#2C3E50", "#16A085"), labels = c("Não usuário","Usuário")) +
-     geom_text(aes(label=..count..),stat = "count", position=position_stack(0.9), color = "black", size = 5) +
-     theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, size = 20),
-           legend.text = element_text(size = 12), axis.text.x = element_text(size = 14)) +
-     scale_x_discrete(labels = NULL)
+base %>% 
+        mutate(DOMAIN = factor(DOMAIN, levels = c("1","2","3","4","5","6")),
+               USERWIKI = factor(USERWIKI, levels = c("0", "1")),
+               ENJ1 = factor(ENJ1, levels = c("1","2","3","4","5")),
+               ENJ2 = factor(ENJ2, levels = c("1","2","3","4","5"))) %>% 
+        filter(!is.na(USERWIKI)) %>% 
+        ggplot(aes(x = as.factor(USERWIKI), fill = as.factor(USERWIKI))) + 
+        geom_bar(na.rm = TRUE) + 
+        labs(title = "Perfil de uso da Wikipedia",
+             x = "", y = NULL) + 
+        scale_fill_manual(values = c("#2C3E50", "#16A085"), labels = c("Não usuário","Usuário")) +
+        geom_text(aes(label=..count..),
+                  stat = "count",
+                  position = position_stack(),
+                  color = "black",
+                  size = 5,
+                  vjust = -0.2) +
+        theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, size = 20),
+              legend.text = element_text(size = 12), axis.text.x = element_text(size = 14),
+              legend.position = "bottom") +
+        scale_x_discrete(labels = NULL)
 
 
 # Idade por gênero
@@ -101,84 +87,101 @@ ggplot(data = base, mapping = aes(x = AGE, fill = as.factor(GENDER))) +
      labs(title = "Idade dos professores entrevistados, por gênero",
           x = "Idade", y = NULL) +
      theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, size = 20),
-           legend.text = element_text(size = 12), axis.text.x = element_text(size = 14)) +
+           legend.text = element_text(size = 12), axis.text.x = element_text(size = 14),
+           legend.position = "bottom") +
      scale_fill_manual(values = c("#F57F17", "#BF360C"), labels = c("Masculino","Feminino"))
 
 
 # Área de atuação
 base %>%
-     filter(!is.na(DOMAIN)) %>% 
-ggplot(aes(x = DOMAIN, fill = as.factor(DOMAIN))) +
-     geom_bar(stat = "count", position = "dodge", na.rm = TRUE) +
-     geom_text(aes(label = ..count..),
-               stat = "count",
-               position = position_stack(),
-               vjust = -0.3,
-               color = "black",
-               size = 4,
-               font) +
-     labs(title = "Área de atuação dos professores",
-          x = "Área", y = NULL) +
-     theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, size = 20),
-           legend.text = element_text(size = 12), axis.text.x = element_text(size = 14),
-           legend.position = "bottom", legend.text.align = 0) +
-     scale_fill_manual(values = c("#004D40","#00695C","#00796B","#00897B","#2980B9","#3498DB"),
-                       labels = c("Artes & Humanidades", "Ciências",
-                                  "Ciências da Saúde", "Engenharia & Arquitetura","Direito & Política", "Outra"))+
-     scale_x_discrete(labels = NULL)
+        mutate(DOMAIN = factor(DOMAIN, levels = c("1","2","3","4","5","6"))) %>% 
+        filter(!is.na(DOMAIN)) %>% 
+        ggplot(aes(x = DOMAIN, fill = as.factor(DOMAIN))) +
+        geom_bar(stat = "count", position = "dodge", na.rm = TRUE) +
+        geom_text(aes(label = ..count..),
+                  stat = "count",
+                  position = position_stack(),
+                  vjust = -0.3,
+                  color = "black",
+                  size = 4) +
+        labs(title = "Área de atuação dos professores",
+             x = "Área", y = NULL) +
+        theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, size = 20),
+              legend.text = element_text(size = 12), axis.text.x = element_text(size = 14),
+              legend.position = "bottom", legend.text.align = 0) +
+        scale_fill_manual(values = c("#004D40","#00695C","#00796B","#00897B","#2980B9","#3498DB"),
+                          labels = c("Artes & Humanidades", "Ciências",
+                                     "Ciências da Saúde", "Engenharia & Arquitetura","Direito & Política", "Outra"))+
+        scale_x_discrete(labels = NULL)
 
 
 ### Satisfação percebida
 # Usuário
 base %>%
-     filter(!is.na(USERWIKI) & !is.na(ENJ1)) %>% 
-     ggplot(mapping = aes(x = ENJ1, fill = USERWIKI)) +
-     geom_bar(na.rm = TRUE) +
-     labs(title = "O uso da Wikipedia estimula a curiosidade",
-          x = NULL, y = "Frequência") +
-     theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, size = 20),
-           legend.text = element_text(size = 12), axis.text.x = element_text(size = 14),
-           legend.position = "bottom", legend.text.align = 0) +
-     scale_fill_manual(values = c("#2C3E50", "#16A085"), labels = c("Não usuário","Usuário"))
+        mutate(DOMAIN = factor(DOMAIN, levels = c("1","2","3","4","5","6")),
+               USERWIKI = factor(USERWIKI, levels = c("0", "1")),
+               ENJ1 = factor(ENJ1, levels = c("1","2","3","4","5")),
+               ENJ2 = factor(ENJ2, levels = c("1","2","3","4","5"))) %>% 
+        filter(!is.na(USERWIKI) & !is.na(ENJ1)) %>% 
+        ggplot(mapping = aes(x = ENJ1, fill = USERWIKI)) +
+        geom_bar(na.rm = TRUE) +
+        labs(title = "O uso da Wikipedia estimula a curiosidade",
+             x = NULL, y = "Frequência") +
+        theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, size = 20),
+              legend.text = element_text(size = 12), axis.text.x = element_text(size = 14),
+              legend.position = "bottom", legend.text.align = 0) +
+        scale_fill_manual(values = c("#2C3E50", "#16A085"), labels = c("Não usuário","Usuário"))
 
 base %>%
-     filter(!is.na(USERWIKI) & !is.na(ENJ2)) %>% 
-     ggplot(mapping = aes(x = ENJ2, fill = as.factor(USERWIKI))) +
-     geom_bar(na.rm = TRUE) +
-     labs(title = "O uso da Wikipedia é divertido",
-          x = NULL, y = "Frequência") +
-     theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, size = 20),
-           legend.text = element_text(size = 12), axis.text.x = element_text(size = 14),
-           legend.position = "bottom", legend.text.align = 0) +
-     scale_fill_manual(values = c("#2C3E50", "#16A085"), labels = c("Não usuário","Usuário"))
+        mutate(DOMAIN = factor(DOMAIN, levels = c("1","2","3","4","5","6")),
+               USERWIKI = factor(USERWIKI, levels = c("0", "1")),
+               ENJ1 = factor(ENJ1, levels = c("1","2","3","4","5")),
+               ENJ2 = factor(ENJ2, levels = c("1","2","3","4","5"))) %>% 
+        filter(!is.na(USERWIKI) & !is.na(ENJ2)) %>% 
+        ggplot(mapping = aes(x = ENJ2, fill = as.factor(USERWIKI))) +
+        geom_bar(na.rm = TRUE) +
+        labs(title = "O uso da Wikipedia é divertido",
+             x = NULL, y = "Frequência") +
+        theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, size = 20),
+              legend.text = element_text(size = 12), axis.text.x = element_text(size = 14),
+              legend.position = "bottom", legend.text.align = 0) +
+        scale_fill_manual(values = c("#2C3E50", "#16A085"), labels = c("Não usuário","Usuário"))
 
 
 # Área de atuação
 base %>% 
-     filter(!is.na(DOMAIN) & !is.na(ENJ1)) %>% 
-     ggplot(aes(x = ENJ1, fill = as.factor(DOMAIN))) +
-     geom_bar(na.rm = TRUE) +
-     labs(title = "O uso da Wikipedia\nestimula a curiosidade",
-          x = "Escala (1 a 5)", y = "Frequência") +
-     scale_fill_brewer(palette = "Reds",
-                       labels = c("Artes & Humanidades", "Ciências",
-                                  "Ciências da Saúde", "Engenharia & Arquitetura","Direito & Política", "Outra")) +
-     theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, size = 20),
-           legend.text = element_text(size = 12), axis.text.x = element_text(size = 14),
-           legend.position = "bottom", legend.text.align = 0)
+        mutate(DOMAIN = factor(DOMAIN, levels = c("1","2","3","4","5","6")),
+               USERWIKI = factor(USERWIKI, levels = c("0", "1")),
+               ENJ1 = factor(ENJ1, levels = c("1","2","3","4","5")),
+               ENJ2 = factor(ENJ2, levels = c("1","2","3","4","5"))) %>% 
+        filter(!is.na(DOMAIN) & !is.na(ENJ1)) %>% 
+        ggplot(aes(x = ENJ1, fill = as.factor(DOMAIN))) +
+        geom_bar(na.rm = TRUE) +
+        labs(title = "O uso da Wikipedia\nestimula a curiosidade",
+             x = "Escala (1 a 5)", y = "Frequência") +
+        scale_fill_brewer(palette = "Reds",
+                          labels = c("Artes & Humanidades", "Ciências",
+                                     "Ciências da Saúde", "Engenharia & Arquitetura","Direito & Política", "Outra")) +
+        theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, size = 20),
+              legend.text = element_text(size = 12), axis.text.x = element_text(size = 14),
+              legend.position = "bottom", legend.text.align = 0)
 
 base %>%
-     filter(!is.na(DOMAIN) & !is.na(ENJ2)) %>% 
-     ggplot(aes(x = ENJ2, fill = as.factor(DOMAIN))) +
-     geom_bar(na.rm = TRUE) +
-     labs(title = "O uso da Wikipedia \n é divertido",
-          x = "Escala (1 a 5)", y = "Frequência") +
-     scale_fill_brewer(palette = "Reds",
-                       labels = c("Artes & Humanidades", "Ciências",
-                                  "Ciências da Saúde", "Engenharia & Arquitetura","Direito & Política", "Outra")) +
-     theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, size = 20),
-           legend.text = element_text(size = 12), axis.text.x = element_text(size = 14),
-           legend.position = "bottom", legend.text.align = 0)
+        mutate(DOMAIN = factor(DOMAIN, levels = c("1","2","3","4","5","6")),
+               USERWIKI = factor(USERWIKI, levels = c("0", "1")),
+               ENJ1 = factor(ENJ1, levels = c("1","2","3","4","5")),
+               ENJ2 = factor(ENJ2, levels = c("1","2","3","4","5"))) %>% 
+        filter(!is.na(DOMAIN) & !is.na(ENJ2)) %>% 
+        ggplot(aes(x = ENJ2, fill = as.factor(DOMAIN))) +
+        geom_bar(na.rm = TRUE) +
+        labs(title = "O uso da Wikipedia \n é divertido",
+             x = "Escala (1 a 5)", y = "Frequência") +
+        scale_fill_brewer(palette = "Reds",
+                          labels = c("Artes & Humanidades", "Ciências",
+                                     "Ciências da Saúde", "Engenharia & Arquitetura","Direito & Política", "Outra")) +
+        theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, size = 20),
+              legend.text = element_text(size = 12), axis.text.x = element_text(size = 14),
+              legend.position = "bottom", legend.text.align = 0)
 
 
 
@@ -186,13 +189,6 @@ base %>%
 summary(base$AGE)
 summary(base$ENJ1)
 summary(base$ENJ2)
-
-cor(base$USERWIKI, base$ENJ1, use = "na.or.complete")
-cor(base$USERWIKI, base$ENJ2, use = "na.or.complete")
-cor(base$USERWIKI, base$ENJ, use = "na.or.complete")
-
-summary(lm(ENJ ~ USERWIKI, data = base))
-
 
 modelo1 = glm(base$ENJ1Recod ~ base$AGE + base$GENDER + base$DOMAIN + base$PhD + base$YEARSEXP + base$UNIVERSITY + base$UOC_POSITION,
                family = binomial(link = "logit"))
